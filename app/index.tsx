feb08e89742a6a3ce0833ff9d602ab1e3117c73a
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-10-23 07:19:59 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+export default function Index() {
+  const [goals, setGoals] = useState<string[]>([]);
+  const [text, setText] = useState<string>("");
+  const goalInputHandler = (goal: string) => {
+    setText(goal);
+  };
+  const addGoalHandler = () => {
+    setGoals((currentGoals) => [...currentGoals, text]);
+    console.log(goals);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Enter a Goal"
+        />
+        <Button onPress={addGoalHandler} title="Add Goal" />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {goals.map((goal, index) => (
+            <View style={styles.goalItem} key={index}>
+              <Text>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+    paddingBottom: 30,
+  },
+  textInput: {
+    flex: 1,
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+  goalsContainer: {
+    flex: 8,
+    marginTop: 20,
+  },
+  goalItem: {
+    padding: 10,
+    marginVertical: 8,
+    backgroundColor: "#cccccc",
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+});
